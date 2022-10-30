2f3a43ec196da13d7dd6600c0b50f67bf50daf0e
akira19 <akira.tsuruoka.19@gmail.com>
2022-10-30 11:47:58 +0000

    initialize

---
+++ main.js
@@ -0,0 +1,12 @@
+// import { instantiate } from "./lib/rs_lib.generated.js";
+
+// const { add } = await instantiate();
+
+// console.log(add(1, 2));
+
+import { serve } from "https://deno.land/std@0.145.0/http/server.ts";
+import { instantiate } from "https://raw.githubusercontent.com/denoland/wasmbuild_example/324635e55fd38d852ef0b0df93354a447aa07c3b/lib/rs_lib.generated.js";
+
+const { add } = await instantiate();
+
+serve((req: Request) => new Response(add(1, 2)));
